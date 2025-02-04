d4355cfb400fac2ec2c20fa1c76de002e2314007
Armand Niculescu <armand@microanalytics.ro>
2025-02-04 16:51:51 +0200

    Field validation - refactoring

--- utils/schemas.ts
+++ utils/schemas.ts
@@ -5,6 +5,10 @@ export const profileSchema = z.object({
     firstName:z.string()
         .min(2, {message: 'First name must be more than 20 characters'})
         .max(20, {message: 'First name must be less than 20 characters'}),
-    lastName:z.string().max(20, {message: 'Last name must be less than 20 characters'}),
-    userName:z.string().max(20, {message: 'User name must be less than 20 characters'}),
+    lastName:z.string()
+        .min(2, {message: 'Last name must be more than 20 characters'})
+        .max(20, {message: 'Last name must be less than 20 characters'}),
+    userName:z.string()
+        .min(2, {message: 'User name must be more than 20 characters'})
+        .max(20, {message: 'User name must be less than 20 characters'}),
 });
