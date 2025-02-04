d3bfcc20839c9b7aa33f10a4ca97b77508f37f12
Armand Niculescu <armand@microanalytics.ro>
2025-02-04 16:04:47 +0200

    Fetch Profile Image Action

--- components/navbar/UserIcon.tsx
+++ components/navbar/UserIcon.tsx
@@ -1,6 +1,11 @@
 import { LuUser } from 'react-icons/lu';
+import { fetchProfileImage } from '../../utils/actions';
 
-function UserIcon() {
+async function UserIcon() {
+  const profileImage = await fetchProfileImage();
+  if (profileImage) {
+    return <img src={profileImage} className="w-6 h-6 bg-primary rounded-full" />;
+  }
   return <><LuUser className="w-6 h-6 bg-primary rounded-full text-white" /></>;
 }
 
