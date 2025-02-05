a865edd45de16e9b777dffa9d4774154d9856cb4
Armand Niculescu <armand@microanalytics.ro>
2025-02-05 19:52:29 +0200

    Home - Property List

--- components/home/PropertiesContainer.tsx
+++ components/home/PropertiesContainer.tsx
@@ -1,10 +1,25 @@
-function PropertiesContainer({
+import { fetchProperties } from '@/utils/actions';
+import PropertiesList from './PropertiesList';
+import EmptyList from './EmptyList';
+import type { PropertyCardProps } from '@/utils/types';
+
+async function PropertiesContainer({
     category,
     search
 }: {category?: string, search?: string}) {
-  return (
-    <div>PropertiesContainer</div>
-  )
+    const properties: PropertyCardProps[] = await fetchProperties({ category, search });
+
+    if (!properties.length) {
+        return <EmptyList
+            heading='No properties found'
+            message='Try searching for something else or remove filters'
+            btnText='Clear filters'
+        />;
+    }
+
+    return (
+        <PropertiesList properties={properties} />
+    )
 }
 
 export default PropertiesContainer
