2046e57dae447ecf023ed47c58396186e6415fd7
Armand Niculescu <armand@microanalytics.ro>
2025-02-05 17:56:14 +0200

    Empty List

--- components/home/EmptyList.tsx
+++ components/home/EmptyList.tsx
@@ -1,6 +1,19 @@
-function EmptyList() {
+import Link from "next/link"
+import { Button } from "../ui/button"
+
+function EmptyList({
+    heading = 'No items found',
+    message = 'Try searching for something else',
+    btnText = 'Back home'
+}: {heading?: string, message?: string, btnText?: string}) {
   return (
-    <div>EmptyList</div>
+    <div className="mt-4">
+        <h2 className="text-xl font-bold">{heading}</h2>
+        <p className="text-lg">{message}</p>
+        <Button asChild className='mt-4 capitalize'>
+            <Link href="/"> {btnText} </Link>
+        </Button>
+    </div>
   )
 }
 
