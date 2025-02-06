7216bb28f0923c4ffb39b7495b54f3d9cfb1a316
Armand Niculescu <armand@microanalytics.ro>
2025-02-06 09:34:20 +0200

    NavSearch Component

--- components/navbar/NavSearch.tsx
+++ components/navbar/NavSearch.tsx
@@ -1,13 +1,45 @@
-import {Input} from '../ui/input'
+'use client';
+import { Input } from '../ui/input';
+import { useSearchParams, usePathname, useRouter } from 'next/navigation';
+import { useDebouncedCallback } from 'use-debounce';
+import { useState, useEffect } from 'react';
 
 function NavSearch() {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+
+  const [search, setSearch] = useState(searchParams.get('search') || '');
+
+  const handleSearch = useDebouncedCallback((value: string) => {
+    const params = new URLSearchParams(searchParams);
+    if (value) {
+      params.set('search', value);
+    } else {
+      params.delete('search');
+    }
+    replace(`${pathname}?${params.toString()}`);
+  }, 500);
+
+  useEffect(() => {
+    const searchParam = searchParams.get('search');
+    if (!searchParam) {
+      setSearch('');
+    }
+  }, [searchParams]);
+
   return (
     <Input
       type='text'
       placeholder='Search...'
       className='max-w-xs dark:bg-muted'
+      onChange={(e) => {
+        setSearch(e.target.value);
+        handleSearch(e.target.value);
+      }}
+      value={search}
     />
-  )
+  );
 }
 
-export default NavSearch
+export default NavSearch;
