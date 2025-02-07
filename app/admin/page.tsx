4c24c62fa9876051ab1c5cfa0347e68a399a84ff
Armand Niculescu <armand.niculescu@gmail.com>
2025-02-07 22:01:16 +0200

    Admin - Loading Components

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,5 +1,22 @@
-function AdminPage() {
-  return <div>AdminPage</div>;
-}
+import { Suspense } from 'react';
 
+import ChartsContainer from '@/components/admin/ChartsContainer';
+import StatsContainer from '@/components/admin/StatsContainer';
+import {
+  ChartsLoadingContainer,
+  StatsLoadingContainer,
+} from '@/components/admin/Loading';
+
+async function AdminPage() {
+  return (
+    <>
+      <Suspense fallback={<StatsLoadingContainer />}>
+        <StatsContainer />
+      </Suspense>
+      <Suspense fallback={<ChartsLoadingContainer />}>
+        <ChartsContainer />
+      </Suspense>
+    </>
+  );
+}
 export default AdminPage;
